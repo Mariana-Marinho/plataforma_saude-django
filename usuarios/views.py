b106a4bc1f17acecd357dcdfc295f3fda6a923e9
Mariana <mariana.marinhoa@gmail.com>
2024-04-18 01:54:45 -0300

    add cadastro

---
+++ usuarios/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+
+# Create your views here.
+
+def cadastro(request=any):
+    if request.method == "GET":
+        return render(request, "cadastro.html")
+    
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        users = User.objects.filter(username=username)
+
+        if users.exists():
+            messages.add_message(request, constants.ERROR, "O usuário já existe")
+            return redirect('/usuarios/cadastro')
+
+
+        if senha != confirmar_senha:
+            messages.add_message(request, constants.ERROR, "As senhas devem ser iguais")
+            return redirect('/usuarios/cadastro')
+        
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, "A senha deve ter 6 ou mais dígitos")
+            return redirect('/usuarios/cadastro')
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=senha,
+        )
+
+        return redirect('/usuarios/login')
