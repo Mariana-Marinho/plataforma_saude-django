5d9872331b2bf63c5eb379ee2a5a203120ce84f1
Mariana <mariana.marinhoa@gmail.com>
2024-04-21 13:51:21 -0300

    add login

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
-from django.contrib import messages
+from django.contrib import messages, auth
 
 # Create your views here.
 
@@ -37,4 +37,20 @@ def cadastro(request=any):
             password=senha,
         )
 
+        return redirect('/usuarios/login')
+
+def login_view(request=any):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        
+        user = auth.authenticate(request, username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            return redirect('/pacientes/home')
+        
+        messages.add_message(request, constants.ERROR, 'Usuario ou senha inválidos')
         return redirect('/usuarios/login')
